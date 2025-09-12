8803b170a85fa8b245013da8d14670b05232a10f
Rohit <flux.amv25@gmail.com>
2025-09-12 22:22:25 +0530

    Init 🚀

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,9 +1,8 @@
 const API_CONFIG = {
     
-    BASE_URL:import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/chat',
-    CREATE_ROOM:import.meta.env.VITE_API_CREATE_ROOM || 'http://localhost:3000/chat/create',
-    JOIN_ROOM:import.meta.VITE_API_JOIN_ROOM || 'http://localhost:3000/chat/join/:roomId'
-
+    BASE_URL:import.meta.env.VITE_API_BASE_URL || 'https://chat-app-rect.onrender.com/chat',
+    CREATE_ROOM:import.meta.env.VITE_API_CREATE_ROOM  || 'https://chat-app-rect.onrender.com/chat/create',
+    JOIN_ROOM:import.meta.env.VITE_API_JOIN_ROOM || 'https://chat-app-rect.onrender.com/chat/join'
 }
 
 export default API_CONFIG;
--- server/server.js
+++ server/server.js
@@ -12,14 +12,14 @@ app.use(express.json());
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://chat-app-rct.onrender.com'
+    'https://chat-app-rect.onrender.com'
   ]
 }));
 
 const server = http.createServer(app);
 const io = new Server(server, {
     cors : {
-        origin : ['http://localhost:5173','https://chat-app-rct.onrender.com/']
+        origin : ['http://localhost:5173','https://chat-app-rect.onrender.com']
     }
 });
 dotenv.config();
